ebad3a0e1a3e7973ff07824481caafa98b7562d5
chuang93 <chuang93@gmail.com>
2017-03-15 12:35:54 -0700

    introduced server side rendering to the index.jsx route and it works. notice the 'data-ract' property when inspecting server side rendered elements in the browser DOM.

--- MyNodeApp/routes/index.jsx
+++ MyNodeApp/routes/index.jsx
@@ -1,15 +1,29 @@
 ﻿import express from 'express';
 import AppRoutes from '../scripts/jsx/AppRoutes.jsx';
+import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import {match, createRoutes, RouterContext} from 'react-router';
+import {StaticRouter } from 'react-router';
+// now supports server side rendering
 
 var router = express.Router();
 
-const context = {} //no context for now.
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+	const context = {}; //no context for now.
+	const htmlRendered= ReactDOMServer.renderToString(
+		<StaticRouter
+		 location ={req.url}
+		 context = {context}>
+		 	<AppRoutes/>
+		</StaticRouter>
+		);
+    res.render('index', 
+    	{ title: 'Express',
+    	   appLayout: htmlRendered });
+    //res.send(htmlRendered);
 });
 
 module.exports = router;
